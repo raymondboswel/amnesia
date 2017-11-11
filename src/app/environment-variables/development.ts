f2a5e12799364f2bae6ce178221ac345b5742f5b
Raymond Boswel <raymondboswel@gmail.com>
2017-11-11 09:55:56 +0200

    Added ionic proxy for google search for faster development

--- src/app/environment-variables/development.ts
+++ src/app/environment-variables/development.ts
@@ -1,6 +1,7 @@
 export const devVariables = {
     // apiEndpoint: 'http://localhost:4000',
     apiEndpoint: 'http://52.40.98.193:4200',
+    googleSearchEndPoint: 'http://localhost:8100/suggestqueries.google.com',
     environmentName: 'Development Environment',
     ionicEnvName: 'dev'
   };
